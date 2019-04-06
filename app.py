260551987b9cc2eef1452149fa646ac6b0789904
hoparkgo9ma <me@ho9.me>
2019-04-06 18:46:15 +0900

    fix code quaility issue

--- app.py
+++ app.py
@@ -9,16 +9,12 @@ code = open('봇', encoding = 'utf-8').read()
 code = code.replace('메시지 도착하면:', dic['str']['message_recevied'])
 code = re.sub(
     dic['re']['if_message'],
-    dic['str']['if_message'].replace('%_variable_%', 
-        re.compile(dic['re']['if_message']).search(code).group('content')
-    ),
+    dic['str']['if_message'].replace('%_variable_%', re.compile(dic['re']['if_message']).search(code).group('content')),
     code
 )
 code = re.sub(
     dic['re']['send_message'],
-    dic['str']['send_message'].replace('%_variable_%', 
-        re.compile(dic['re']['send_message']).search(code).group('content')
-    ),
+    dic['str']['send_message'].replace('%_variable_%', re.compile(dic['re']['send_message']).search(code).group('content')),
     code
 )
 
